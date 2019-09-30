a0e02dad61dbb17f5fd2e5a093a5169187ac0fcf
VIANEYOMEN <51094232+VIANEYOMEN@users.noreply.github.com>
2019-09-30 16:54:23 -0500

    TAREAS

---
+++ REALIDAD AUMENTADA/Objeto animado/Assets/animacion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animacion : MonoBehaviour
+{
+    Animator animacion;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animacion = GetComponent<animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool caminar = Input.GetKey(KeyCode.LefyControl);
+        animacion.SetBool("Caminar", caminar);
+
+        if(Input.Input.GetKey(KeyCode.Space))
+        {
+            animacion.SetTrigger("ataque");
+        }
+  
+    }
+}
